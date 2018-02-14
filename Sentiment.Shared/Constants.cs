4e625ae451f3cd94b3ac71cb9a5c8fb8d44f585c
Dan Waters <dawate@microsoft.com>
2018-02-13 18:24:48 -0600

    Added SignalR
    Added SignalR Core libraries, refactored code

---
+++ Sentiment.Shared/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace Sentiment.Shared
+{
+    public partial class Constants
+    {
+        public const string HUB_URL = "https://sentimentchathub.azurewebsites.net/sentiment";
+        public const string TEXT_SENTIMENT_SUBSCRIPTION_KEY = "";
+    }
+}
---
+++ Sentiment.Shared/Services/SignalRClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Sentiment.Shared.Services
+{
+    public class SignalRClient
+    {
+        private HubConnection hub;
+
+        public event EventHandler<ChatEventArgs> ChatMessageReceived;
+
+        public SignalRClient()
+        {
+            SignalRInit();
+        }
+
+        private async Task SignalRInit()
+        {
+            hub = new HubConnectionBuilder().WithUrl(Constants.HUB_URL).Build();
+
+            hub.On<string, string, double>("NewAnalyzedMessage", 
+                 (username, text, sentiment) => 
+                 ChatMessageReceived?.Invoke(this, new ChatEventArgs(username, text, sentiment)));
+
+            await hub.StartAsync();
+        }
+
+        public async Task SendChatMessage(string username, string message, double sentiment)
+        {
+            await hub.InvokeAsync("NewAnalyzedMessage", new object[] { username, message, sentiment });
+        }
+    }
+}
